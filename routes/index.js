8a2f880c1d8b4f8961bf856a0e08748ed03de5d8
ashishpathak2 <pathak1420@gmail.com>
2024-09-17 12:27:04 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -20,13 +20,22 @@ router.get('/', function (req, res) {
 });
 
 router.post('/link', async function (req, res) {
+  const cookie = "SID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNcLEBbIAaeOcvRiVgfvrdWLQACgYKASUSARQSFQHGX2MiY7k9wpkg690zFZ42fGMlMxoVAUF8yKp-74tndRm_ClX8vVYoKd_-0076; __Secure-1PSID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNcGvFFcYJ-w33K8yL3GiG58AACgYKAYkSARQSFQHGX2MiPAoL-T39KCyDGde60v_ZJxoVAUF8yKoxuhVE34GUaCaYfmNK0Jt10076; __Secure-3PSID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNclZscsV28A_A4WODH-zl99gACgYKARcSARQSFQHGX2MiKUZfAHvEX252c_5WLUZrBhoVAUF8yKo0erxLfEqDgGAzemfAPn5C0076; HSID=At0nFZP20pzR2O0Vy; SSID=ALPSQMMtCngD3DjRe; APISID=_tqtJraCzhbSxLKs/AO-S_mGFU7In8vbxk; SAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; __Secure-1PAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; __Secure-3PAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; VISITOR_INFO1_LIVE=Jm37A2d_OGU; VISITOR_PRIVACY_METADATA=CgJJThIEGgAgDQ%3D%3D; LOGIN_INFO=AFmmF2swRAIgDJoK4pRFUBLtj38-iab_oyYSM5KE4iqD82Dcm2j9i2ICIC0hxQfX_JjkB-F8E2etSEDmQ1LGO685yEw32hlNLu84:QUQ3MjNmelBjdzdPbS16cFJlaS1vdDlEcnZmeE13VnlPUVh5QXphSEtlVmhQVnlkQnBfSjJyY056LVlyeHh1TFVOOE04bV9iRmxiSmUwU2VwM3loRUJ5aWo5QnhteTBKWG43M2hLbVVwYVcyWVNuRHVnZHdGX1UwMWlsNmZFOVBIUnhUeWRLS0FEVTRXTzFVRUItT2xfTEZJdF83bENZTTdn; OTZ=7735227_34_34__34_; NID=517=w5AhclVP6HP7zPbhrOUa_l4VpW8G1uPy0E1oS3mIa2A19CNKUJJwU_UKL53YRqSoy2HxvidlCpWCTh5mhcUsoimwT5SMCSOSvb9o62s1LL6lCap9oIrAsrRl3Mt--pM_ynzaNO9HLekGmHgHz-Av7WYjhaQHcv8qcPb4tu-HnpsMkTeHOR7S85myKDxPBZJ5LHQTTKSYniz2sjpqHKU_EJl7f81QaMOb9CepiqzJ200kDqJvbxHVjdlrbawYhM8VF1QK6DyyeYA3v1wdjw; YSC=YQLACkwq3Aw; PREF=f4=4000000&f6=40000000&tz=Asia.Calcutta&f7=100&f5=30000; __Secure-1PSIDTS=sidts-CjEBQlrA-CHDqojuxPvIkT4pTQ_m_LzDb4dEBjlGnXXJXyOLiD4Zcc4xC70D_-uIi0-9EAA; __Secure-3PSIDTS=sidts-CjEBQlrA-CHDqojuxPvIkT4pTQ_m_LzDb4dEBjlGnXXJXyOLiD4Zcc4xC70D_-uIi0-9EAA; SIDCC=AKEyXzVvuoxsn96CitEZWFk5LQF71wOYRo8q6E3Qknd8jOqwoEwp0PBtu9axAMHzXQBIxMHAP-0; __Secure-1PSIDCC=AKEyXzVDpqerqeNBYufFuTSKJVY_kZwogqqHickA1c-fMt1CmQ30bXkx1PtFeHVdwWNRyNFWL5M; __Secure-3PSIDCC=AKEyXzWVJUD2fbAOtHTbnPjKX1JD4iJEodYc2tXAB8D-HZIdW3mxVmU4YVBvHfwf3vkMTVEWjA";
+
   url = req.body.url;
   if (!url || !ytdl.validateURL(url)) {
     return res.redirect('/');
   }
 
   try {
-    const info = await ytdl.getInfo(url);
+    const info = await ytdl.getInfo(url ,{
+      requestOptions: {
+        headers: {
+          Cookie: cookie, // Add the extracted cookie here
+          proxy: `http://localhost:${process.env.PORT}`
+        }
+      }
+    });
     const qualityLabel = dataFilter(info);
     const uniqueVideoDetails = removeDuplicatesByQualityLabel(qualityLabel);
 
@@ -45,6 +54,8 @@ router.post('/link', async function (req, res) {
 });
 
 router.post('/download', async function (req, res) {
+  const cookie = "SID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNcLEBbIAaeOcvRiVgfvrdWLQACgYKASUSARQSFQHGX2MiY7k9wpkg690zFZ42fGMlMxoVAUF8yKp-74tndRm_ClX8vVYoKd_-0076; __Secure-1PSID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNcGvFFcYJ-w33K8yL3GiG58AACgYKAYkSARQSFQHGX2MiPAoL-T39KCyDGde60v_ZJxoVAUF8yKoxuhVE34GUaCaYfmNK0Jt10076; __Secure-3PSID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNclZscsV28A_A4WODH-zl99gACgYKARcSARQSFQHGX2MiKUZfAHvEX252c_5WLUZrBhoVAUF8yKo0erxLfEqDgGAzemfAPn5C0076; HSID=At0nFZP20pzR2O0Vy; SSID=ALPSQMMtCngD3DjRe; APISID=_tqtJraCzhbSxLKs/AO-S_mGFU7In8vbxk; SAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; __Secure-1PAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; __Secure-3PAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; VISITOR_INFO1_LIVE=Jm37A2d_OGU; VISITOR_PRIVACY_METADATA=CgJJThIEGgAgDQ%3D%3D; LOGIN_INFO=AFmmF2swRAIgDJoK4pRFUBLtj38-iab_oyYSM5KE4iqD82Dcm2j9i2ICIC0hxQfX_JjkB-F8E2etSEDmQ1LGO685yEw32hlNLu84:QUQ3MjNmelBjdzdPbS16cFJlaS1vdDlEcnZmeE13VnlPUVh5QXphSEtlVmhQVnlkQnBfSjJyY056LVlyeHh1TFVOOE04bV9iRmxiSmUwU2VwM3loRUJ5aWo5QnhteTBKWG43M2hLbVVwYVcyWVNuRHVnZHdGX1UwMWlsNmZFOVBIUnhUeWRLS0FEVTRXTzFVRUItT2xfTEZJdF83bENZTTdn; OTZ=7735227_34_34__34_; NID=517=w5AhclVP6HP7zPbhrOUa_l4VpW8G1uPy0E1oS3mIa2A19CNKUJJwU_UKL53YRqSoy2HxvidlCpWCTh5mhcUsoimwT5SMCSOSvb9o62s1LL6lCap9oIrAsrRl3Mt--pM_ynzaNO9HLekGmHgHz-Av7WYjhaQHcv8qcPb4tu-HnpsMkTeHOR7S85myKDxPBZJ5LHQTTKSYniz2sjpqHKU_EJl7f81QaMOb9CepiqzJ200kDqJvbxHVjdlrbawYhM8VF1QK6DyyeYA3v1wdjw; YSC=YQLACkwq3Aw; PREF=f4=4000000&f6=40000000&tz=Asia.Calcutta&f7=100&f5=30000; __Secure-1PSIDTS=sidts-CjEBQlrA-CHDqojuxPvIkT4pTQ_m_LzDb4dEBjlGnXXJXyOLiD4Zcc4xC70D_-uIi0-9EAA; __Secure-3PSIDTS=sidts-CjEBQlrA-CHDqojuxPvIkT4pTQ_m_LzDb4dEBjlGnXXJXyOLiD4Zcc4xC70D_-uIi0-9EAA; SIDCC=AKEyXzVvuoxsn96CitEZWFk5LQF71wOYRo8q6E3Qknd8jOqwoEwp0PBtu9axAMHzXQBIxMHAP-0; __Secure-1PSIDCC=AKEyXzVDpqerqeNBYufFuTSKJVY_kZwogqqHickA1c-fMt1CmQ30bXkx1PtFeHVdwWNRyNFWL5M; __Secure-3PSIDCC=AKEyXzWVJUD2fbAOtHTbnPjKX1JD4iJEodYc2tXAB8D-HZIdW3mxVmU4YVBvHfwf3vkMTVEWjA";
+
   const qualityLabel = req.body.quality;
   if (!qualityLabel) {
     return res.redirect('/');
@@ -62,7 +73,14 @@ router.post('/download', async function (req, res) {
   const itag = itagMap[qualityLabel];
 
   try {
-    const info = await ytdl.getInfo(url);
+    const info = await ytdl.getInfo(url ,{
+      requestOptions: {
+        headers: {
+          Cookie: cookie, // Add the extracted cookie here
+          proxy: `http://localhost:${process.env.PORT}`
+        }
+      }
+    });
     const sanitizedTitle = sanitizeFileName(info.videoDetails.title).replace(/[<>:"\/\\|?*]+/g, '_').substring(0, 100);
 
     // Create temporary file paths
@@ -70,7 +88,6 @@ router.post('/download', async function (req, res) {
     const audioPath = path.resolve(tempDir, `${sanitizedTitle}_audio.mp4`);
     const outputFile = path.resolve(tempDir, `${sanitizedTitle}_merged.mp4`);
 
-    const cookie = "SID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNcLEBbIAaeOcvRiVgfvrdWLQACgYKASUSARQSFQHGX2MiY7k9wpkg690zFZ42fGMlMxoVAUF8yKp-74tndRm_ClX8vVYoKd_-0076; __Secure-1PSID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNcGvFFcYJ-w33K8yL3GiG58AACgYKAYkSARQSFQHGX2MiPAoL-T39KCyDGde60v_ZJxoVAUF8yKoxuhVE34GUaCaYfmNK0Jt10076; __Secure-3PSID=g.a000oAj2ZEBsKYu8PiNfdOJrqku1wOqy0uaVV9Mn8WC0qVGKSbNclZscsV28A_A4WODH-zl99gACgYKARcSARQSFQHGX2MiKUZfAHvEX252c_5WLUZrBhoVAUF8yKo0erxLfEqDgGAzemfAPn5C0076; HSID=At0nFZP20pzR2O0Vy; SSID=ALPSQMMtCngD3DjRe; APISID=_tqtJraCzhbSxLKs/AO-S_mGFU7In8vbxk; SAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; __Secure-1PAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; __Secure-3PAPISID=dBFwRulJjfwihxmR/AQr1A1Rum0nk6jZSx; VISITOR_INFO1_LIVE=Jm37A2d_OGU; VISITOR_PRIVACY_METADATA=CgJJThIEGgAgDQ%3D%3D; LOGIN_INFO=AFmmF2swRAIgDJoK4pRFUBLtj38-iab_oyYSM5KE4iqD82Dcm2j9i2ICIC0hxQfX_JjkB-F8E2etSEDmQ1LGO685yEw32hlNLu84:QUQ3MjNmelBjdzdPbS16cFJlaS1vdDlEcnZmeE13VnlPUVh5QXphSEtlVmhQVnlkQnBfSjJyY056LVlyeHh1TFVOOE04bV9iRmxiSmUwU2VwM3loRUJ5aWo5QnhteTBKWG43M2hLbVVwYVcyWVNuRHVnZHdGX1UwMWlsNmZFOVBIUnhUeWRLS0FEVTRXTzFVRUItT2xfTEZJdF83bENZTTdn; OTZ=7735227_34_34__34_; NID=517=w5AhclVP6HP7zPbhrOUa_l4VpW8G1uPy0E1oS3mIa2A19CNKUJJwU_UKL53YRqSoy2HxvidlCpWCTh5mhcUsoimwT5SMCSOSvb9o62s1LL6lCap9oIrAsrRl3Mt--pM_ynzaNO9HLekGmHgHz-Av7WYjhaQHcv8qcPb4tu-HnpsMkTeHOR7S85myKDxPBZJ5LHQTTKSYniz2sjpqHKU_EJl7f81QaMOb9CepiqzJ200kDqJvbxHVjdlrbawYhM8VF1QK6DyyeYA3v1wdjw; YSC=YQLACkwq3Aw; PREF=f4=4000000&f6=40000000&tz=Asia.Calcutta&f7=100&f5=30000; __Secure-1PSIDTS=sidts-CjEBQlrA-CHDqojuxPvIkT4pTQ_m_LzDb4dEBjlGnXXJXyOLiD4Zcc4xC70D_-uIi0-9EAA; __Secure-3PSIDTS=sidts-CjEBQlrA-CHDqojuxPvIkT4pTQ_m_LzDb4dEBjlGnXXJXyOLiD4Zcc4xC70D_-uIi0-9EAA; SIDCC=AKEyXzVvuoxsn96CitEZWFk5LQF71wOYRo8q6E3Qknd8jOqwoEwp0PBtu9axAMHzXQBIxMHAP-0; __Secure-1PSIDCC=AKEyXzVDpqerqeNBYufFuTSKJVY_kZwogqqHickA1c-fMt1CmQ30bXkx1PtFeHVdwWNRyNFWL5M; __Secure-3PSIDCC=AKEyXzWVJUD2fbAOtHTbnPjKX1JD4iJEodYc2tXAB8D-HZIdW3mxVmU4YVBvHfwf3vkMTVEWjA";
     // Download video and audio separately to temp files
     const videoStream = ytdl(url, {
       quality: itag,
